4d2a979f35b989e86a286bef383589c80941daaa
Enzo Maruffa <enzomm1999@gmail.com>
2021-11-16 23:51:20 -0300

    Adds a version that uses MPI for no reason but works so far

---
+++ main/parallel_batch.sh
@@ -0,0 +1,43 @@
+export TIMEFORMAT=%R
+INPUT_SIZES=(100 1000 2000 4000 5000 8000 10000 12000 15000 20000 25000 30000)
+TIMESTEPS=(50)
+THREADS=(1 2 4 8 12)
+ITERATIONS=30
+
+mkdir -p .temp
+mkdir -p data
+
+printf "particle_count,timesteps,threads,sample,time\n" > data/times_results.txt
+
+for INPUT in ${INPUT_SIZES[*]}; do
+    for TIMESTEP in ${TIMESTEPS[*]}; do
+        for THREAD_COUNT in ${THREADS[*]}; do
+            export OMP_NUM_THREADS=$THREAD_COUNT
+            for ITERATION in `seq $ITERATIONS`; do
+                echo "Running ($INPUT, $TIMESTEP, $THREAD_COUNT) iteration $ITERATION"
+                
+                printf "${INPUT}\n${TIMESTEP}\n" > .temp/parallel_batch_input.in
+
+                RUN_TIME=$({ time ./nbody < .temp/parallel_batch_input.in >.temp/temp_output.txt; } 2>&1)
+
+                diff .temp/temp_output.txt ../original/outputs/${INPUT}_${TIMESTEP}.txt > .temp/temp_diff.txt
+
+                if [ -s .temp/temp_diff.txt ]; then
+                    # The file is not-empty. Add to errors.
+                    mkdir -p errors
+                    echo "  - Error!"
+                    printf "Error for input ${INPUT}/${TIMESTEP} with ${THREAD_COUNT} threads in iteration ${ITERATION}\n" >> errors/errors.log
+                    # mv .temp/temp_output.txt errors/${INPUT}_${TIMESTEP}_$THREAD_COUNT.out
+                    # mv .temp/temp_diff.txt errors/${INPUT}_${TIMESTEP}_$THREAD_COUNT.err
+                fi
+
+                printf "${INPUT},${TIMESTEP},${THREAD_COUNT},${ITERATION},${RUN_TIME}\n" >> data/times_results.txt
+            done
+        done
+    done
+done
+
+rm .temp/parallel_batch_input.in
+rm .temp/temp_output.txt
+rm .temp/temp_diff.txt
+rm -rf .temp
---
+++ original/sequential_batch.sh
@@ -0,0 +1,33 @@
+export TIMEFORMAT=%R
+INPUT_SIZES=(100 1000 2000 4000 5000 8000 10000 12000 15000 20000 25000 30000)
+TIMESTEPS=(50)
+ITERATIONS=30
+
+touch temp_time.txt
+
+mkdir -p outputs
+mkdir -p data
+
+printf "particle_count,timesteps,sample,time\n" > data/times_results.txt
+
+for INPUT in ${INPUT_SIZES[*]}; do
+    for TIMESTEP in ${TIMESTEPS[*]}; do
+        for ITERATION in `seq $ITERATIONS`; do
+            echo "Running ($INPUT, $TIMESTEP) iteration $ITERATION"
+            touch "outputs/${INPUT}_${TIMESTEP}.txt"
+
+            printf "${INPUT}\n${TIMESTEP}\n" > sequential_batch_input.in
+
+            if [ $ITERATION -eq 1 ]; then
+                RUN_TIME=$({ time ./nbody < sequential_batch_input.in >"outputs/${INPUT}_${TIMESTEP}.txt"; } 2>&1)
+            else
+                RUN_TIME=$({ time ./nbody < sequential_batch_input.in >/dev/null; } 2>&1)
+            fi
+
+            printf "${INPUT},${TIMESTEP},${ITERATION},${RUN_TIME}\n" >> data/times_results.txt
+        done
+    done
+done
+
+rm temp_time.txt
+rm sequential_batch_input.in
